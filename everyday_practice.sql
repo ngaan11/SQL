66011078b200423b4a7bbcb5ee8d069e5e9b67b9
ngaan11 <153655045+ngaan11@users.noreply.github.com>
2024-03-15 22:22:41 +0200

    Update everyday_practice.sql

--- everyday_practice.sql
+++ everyday_practice.sql
@@ -2,3 +2,8 @@
 SELECT tweet_id
 FROM Tweets
 WHERE char_length(content) > 15;
+
+--Basic joins 1378
+SELECT Employees.name, EmployeeUNI.unique_id
+FROM Employees
+LEFT JOIN EmployeeUNI ON Employees.id=EmployeeUNI.id;
