da2cc00e8b496c48bec3ddf47bc4d767671cd63d
ngaan11 <153655045+ngaan11@users.noreply.github.com>
2024-03-20 00:01:16 +0200

    Update everyday_practice.sql

--- everyday_practice.sql
+++ everyday_practice.sql
@@ -3,7 +3,7 @@ SELECT tweet_id
 FROM Tweets
 WHERE char_length(content) > 15;
 
---Basic joins 1378
+--Basic joins #1378
 SELECT Employees.name, EmployeeUNI.unique_id
 FROM Employees
 LEFT JOIN EmployeeUNI ON Employees.id=EmployeeUNI.id;
@@ -13,7 +13,7 @@ SELECT product_name, year, price
 FROM Sales
 LEFT JOIN Product ON Sales.product_id=Product.product_id; 
 
---Basic joins 1581
+--Basic joins #1581
 SELECT 
     a.customer_id, 
     COUNT(a.visit_id) as count_no_trans
@@ -27,8 +27,8 @@ WHERE
     amount IS NULL
 GROUP BY 
     a.customer_id;
-#570
-# Write your MySQL query statement below
+
+-- Basic joins #570
 WITH t AS (SELECT managerId
 FROM Employee
 GROUP BY managerId
